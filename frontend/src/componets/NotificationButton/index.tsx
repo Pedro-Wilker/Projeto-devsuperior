3ccdc5936c97157879cd97dd2e6a70b442103a35
Pedro <pedrowilker1516@gmail.com>
2022-09-10 19:14:55 -0300

    Add notification in the front end

--- frontend/src/componets/NotificationButton/index.tsx
+++ frontend/src/componets/NotificationButton/index.tsx
@@ -1,9 +1,20 @@
 import icon from '../../assets/img/notification-icon.svg';
 import "./style.css";
+import axios from "axios";
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton(){
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id: number){
+    axios(`${BASE_URL}/sales/${id}/notification`).then(response => {
+        console.log("sucesso");
+    });}
+
+function NotificationButton({saleId} : Props){
     return (
-        <div className="dsmeta-red-bnt">
+        <div className="dsmeta-red-bnt" onClick={()=> handleClick(saleId)}>
             <img src={icon} alt="Notificar"/>
         </div>
     )
